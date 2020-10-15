585e46fdf0082e0b59afe4ae0c9c5a89ed005e2a
Seonghyeon <han041220@gmail.com>
2020-10-16 00:21:51 +0800

    전체적인 리뉴얼

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// 전역변수 선언
+const aTags = document.querySelectorAll("nav ul li a");
+const sections = document.querySelectorAll("#container > section");
+
+function main() {
+    // a 태그를 선택하면 할일
+    for (let i = 0; i < aTags.length; i++) {
+        aTags[i].addEventListener("click", handleClick);
+    }
+
+    // 스크롤시 할일
+    window.addEventListener("scroll", handleScroll);
+}
+
+function handleClick(event) {
+    const index = event.target.getAttribute("data-index");
+    const sectionY = sections[index].offsetTop;
+
+    window.scrollTo({
+        top: sectionY,
+        left: 0,
+
+        behavior: "smooth"
+    });
+}
+
+function handleScroll() {
+    const currentY = window.pageYOffset;
+
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i].offsetTop <= currentY) {
+            let index = sections[i].getAttribute("data-index");
+
+            // 모든 a 태그의 active 클래스를 제거하고, index에 해당하는 번호만 active 클래스 추가
+            for (let j = 0; j < aTags.length; j++) {
+                aTags[j].classList.remove("active");
+            }
+
+            aTags[index].classList.add("active");
+        }
+    }
+}
+
+main();
